7db3f64c47cd54cb6855d7932d427570c7ca87d5
Gizem <109289446+gizemayegul@users.noreply.github.com>
2025-09-07 10:50:27 +0200

    builder pattern

--- src/main/java/com/example/ItemRegistry.java
+++ src/main/java/com/example/ItemRegistry.java
@@ -4,13 +4,14 @@ public class ItemRegistry {
 
     public static void main(String[] args) {
 
-        var item1 = new StoreItem(
-                "Pretzel", 2.0, "A tasty snack", null, 7, null);
+        var item1 = new StoreItem.StoreItemBuilder(
+                "Pretzel", 2.0).withShortDescription("A tasty snack").withStockAvailable(7).build();
 
-        var item2 = new StoreItem(
-                "Soup", 1.5, null,
-                "A meal that you can warm up at home. It can come in different flavours including tomato, chicken, and vegetable",
-                null, "Can");
+        var item2 = new StoreItem.StoreItemBuilder(
+                "Soup", 1.5)
+                .withLongDescription(
+                        "A meal that you can warm up at home. It can come in different flavours including tomato, chicken, and vegetable")
+                .withPackagingType("Can").build();
 
         addToItemRegistry(item1);
         addToItemRegistry(item2);
